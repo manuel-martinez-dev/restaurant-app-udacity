5a4ba1aab45702863b182dd4a0f27dc985a3bf11
Manuel-Alejandro-Martinez <manuel.martinez59000@gmail.com>
2019-01-02 20:27:08 +0100

    4th commit - added service worker

---
+++ js/SW-register.js
@@ -0,0 +1,7 @@
+if (navigator.serviceWorker){
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log("Successfull Service Worker registration");
+  }).catch((event) =>{
+    console.log("could not register Service Worker", event);
+  });
+}
---
+++ sw.js
@@ -0,0 +1,40 @@
+const appName = 'v1';
+const cacheFiles = [
+  '/',
+  '/restaurant.html',
+  '/css/medium.css',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/map-API.js',
+  '/js/restaurant_info.js',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(appName).then(function(cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function(response) {
+            const responseClone = response.clone();
+            caches.open(appName).then(function(cache) {
+              cache.put(event.request, responseClone);
+            })
+            return response;
+          }).catch(function(err) {
+            console.error(err);
+          });
+      }
+    })
+  );
+});
